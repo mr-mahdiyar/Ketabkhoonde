48acca8c22f488a1c74c9993c472adce68bca26e
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-12 16:29:39 +0330

    custom theme was added.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import myTheme from "./utils/Theme.jsx";
+import { ThemeProvider } from "@mui/material/styles";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={myTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/utils/Theme.jsx
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+const myTheme = createTheme({
+    palette: {
+        customBlue: {
+            main: "#00c0e2"
+        },
+        customGreen: {
+            main: "#50DBB4"
+        },
+        customGray: {
+            main: "#dcdde1"
+        },
+        white: {
+            main: "#ffffff"
+        }
+    }
+})
+
+export default myTheme;
