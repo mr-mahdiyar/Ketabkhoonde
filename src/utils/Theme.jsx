f52b0126b8add94df97ac1964b4653f610532430
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-16 16:34:39 +0330

    Custom font names were added.

--- src/utils/Theme.jsx
+++ src/utils/Theme.jsx
@@ -1,20 +1,28 @@
 import { createTheme } from "@mui/material";
-
 const myTheme = createTheme({
-    palette: {
-        customBlue: {
-            main: "#00c0e2"
-        },
-        customGreen: {
-            main: "#50DBB4"
-        },
-        customGray: {
-            main: "#dcdde1"
-        },
-        white: {
-            main: "#ffffff"
-        }
-    }
-})
+  typography: {
+    fontFamily: ["Dirooz", "Diplomat"].join(',')
+  },
+  palette: {
+    customBlue: {
+      main: "#00c0e2",
+    },
+    customGreen: {
+      main: "#50DBB4",
+    },
+    customGray: {
+      main: "#dcdde1",
+    },
+    white: {
+      main: "#ffffff",
+    },
+    black: {
+      main: "#000000",
+    },
+    customeOrange: {
+      main: "#fe4902",
+    },
+  },
+});
 
 export default myTheme;
