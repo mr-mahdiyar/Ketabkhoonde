34923ab141be500522f746dbaa36b887ac46bd70
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-20 05:17:11 +0330

    The slider became responsive.

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,36 +9,48 @@ const staticSliderItem = () => {
       className="image-text-container"
       component="div"
       display="flex"
+      flexDirection={{xs: "column", sm: "row"}}
       justifyContent="space-between"
       alignItems="center"
       border="1px solid white"
       padding={2}
       borderRadius={3}
     >
-      <Box className="image-container" maxWidth="40%">
+      <Box className="image-container" maxWidth={{xs: "75%" ,sm: "40%"}}>
         <img src={Image} className="w-100" />
       </Box>
       <Box className="text-wrapper" minWidth="50%" marginLeft="1rem">
         <List>
           <ListItem>
             <ListItemText primary="نام کتاب: " />
-            <ListItemText primary="دنیای سوفی" sx={{textAlign: "start", marginRight: "0.4rem"}}/>
+            <ListItemText
+              primary="دنیای سوفی"
+              primaryTypographyProps={{textAlign: "start", marginRight: "0.4rem", fontSize: {xs: "16px", sm: "18px"}}}
+            />
           </ListItem>
           <ListItem>
-            <ListItemText primary="نویسنده: "/>
-            <ListItemText primary="بوستین گردر" sx={{textAlign: "start", marginRight: "0.4rem"}}/>
+            <ListItemText primary="نویسنده: " />
+            <ListItemText
+              primary="بوستین گردر"
+              primaryTypographyProps={{textAlign: "start", marginRight: "0.4rem", fontSize: {xs: "16px", sm: "18px"}}}
+            />
           </ListItem>
           <ListItem>
             <ListItemText
               primary="تعداد صفحات: "
               sx={{ marginLeft: "0.1rem" }}
             />
-            <ListItemText primary="325" sx={{textAlign: "start", marginRight: "0.4rem"}}/>
+            <ListItemText
+              primary="325"
+              primaryTypographyProps={{textAlign: "start", marginRight: "0.4rem", fontSize: {xs: "16px", sm: "18px"}}}
+            />
           </ListItem>
           <ListItem>
             <ListItemText primary="قیمت: " />
-            <ListItemText primary="324 هزار تومان" sx={{textAlign: "start", marginRight: "0.4rem"}} />
-            
+            <ListItemText
+              primary="324 هزار تومان"
+              primaryTypographyProps={{textAlign: "start", marginRight: "0.4rem", fontSize: {xs: "16px", sm: "18px"}}}
+            />
           </ListItem>
         </List>
       </Box>
@@ -62,6 +74,17 @@ const Slider = () => {
           centeredSlides
           centeredSlidesBounds
           freeMode
+          breakpoints={{
+            300: {
+              slidesPerView: 1.4
+            }, 
+            900: {
+              slidesPerView: 1.75
+            },
+            1300: {
+              slidesPerView: 3.5
+            }
+          }}
         >
           <SwiperSlide>{staticSliderItem()}</SwiperSlide>
 
